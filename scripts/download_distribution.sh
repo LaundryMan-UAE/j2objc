6fc748febc043761cf76d2bb32776c5819f52029
Michael Voong <michael@laundrapp.com>
2015-03-30 12:21:03 +0100

    Updated script for 0.9.6.3 release

--- scripts/download_distribution.sh
+++ scripts/download_distribution.sh
@@ -15,15 +15,13 @@ set -ev
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-j2objc_version=0.9.5
-sha1_checksum=35a526406b3f4c1847fa1f93ca6237e5e4170d38
+j2objc_version=0.9.6.3
 
 if [[ -d dist ]]; then
   exit
 fi
 
 echo "fetching j2objc"
-curl -OL https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
-#echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
+curl -OL https://github.com/hambroperks/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} dist
